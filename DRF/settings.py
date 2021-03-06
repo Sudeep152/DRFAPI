bae6aa39ba2d076f0ce9bb44d4fcc56795247b0a
Shashank <sudeepdahiya152@gmail.com>
2021-03-06 13:45:23 +0530

    sudeep

--- DRF/settings.py
+++ DRF/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@o*hfqx3_r)t#mj=4#7kgkn7zk@ijqd7zbwk1(b0%&63($hw7!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','shashank.com']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
